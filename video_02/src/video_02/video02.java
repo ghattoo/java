795d2f670be052653b94dafbc3ddc3518a361870
ghattoo <toth.79.peter@gmail.com>
2019-03-03 12:13:04 +0100

    20190303-halapon

---
+++ video_02/src/video_02/video02.java
@@ -0,0 +1,28 @@
+package video_02;
+
+public class video02 {
+
+	public static void main(String[] args) {
+		//System.out.println("Hello Wilág!!!");
+
+		int szam1 = 5;
+		int szam2 = 10;
+		int szam3;
+		int osszeg = szam1 + szam2;
+		int az_En_Szamom_1248 = 1979;
+		int x = 10;
+		int X = 20;
+		System.out.println(szam1);
+		
+		szam1 = 15;
+		System.out.println(szam1);
+		System.out.println(osszeg);
+		szam3 = 40;
+		System.out.println(az_En_Szamom_1248);
+		System.out.println(az_En_Szamom_1248+szam3);
+		System.out.println(x);
+		System.out.println(X);
+		
+	}
+
+}
